7c5b006205316b71508806103956b6fa6a7875c2
nikotpbn <niko@Nicolaus-MacBook-Pro.local>
2025-05-29 12:59:42 +0100

    requirements and settings

---
+++ backend/backend/settings/local.py
@@ -0,0 +1,23 @@
+from os import getenv
+
+from .base import *  # noqa
+from .base import BASE_DIR
+
+SECRET_KEY = getenv("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = getenv("DEBUG")
+
+SITE_NAME = getenv("SITE_NAME")
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("ADMIN_URL")
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
+
+MAX_UPLOAD_SIZE = 1 * 1024 * 1024
